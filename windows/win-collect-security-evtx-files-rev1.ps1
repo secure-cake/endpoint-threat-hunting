97bd50887237c5feee57c1a9f4154e44241692f6
Secure Cake <49917607+secure-cake@users.noreply.github.com>
2026-06-04 10:45:13 -0600

    Update win-collect-security-evtx-files-rev1.ps1

--- windows/win-collect-security-evtx-files-rev1.ps1
+++ windows/win-collect-security-evtx-files-rev1.ps1
@@ -1,8 +1,11 @@
+#NOTE: You'll need to be logged in with an account that has adequate permissions for remote PoSh and EVTX access
 $timestamp = Get-Date -Format "yyyyMMdd_HHmm"
+#Change to match your desired output directory path
 $outputDir = "d:\cases\case-xyz\evtx\$timestamp"
 
 If (!(Test-Path $outputDir)) { New-Item -ItemType Directory -Path $outputDir -Force }
 
+#Change to match your desired OU/Domain query or alternate "$servers" variable
 $servers = (Get-ADComputer -Filter {Enabled -eq $true} -SearchBase "ou=servers,dc=test,dc=local").Name
 
 Write-Host "Servers found: $($servers.Count)`n"
